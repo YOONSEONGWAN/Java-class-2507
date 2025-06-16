85dd613b3d310e615e5fd93da4b782378c67d27f
seongwan <wannietheyoon@naver.com>
2025-06-16 16:08:04 +0900

    Hello 프로젝트 추가함

---
+++ Hello/src/test/main/MainClass.java
@@ -0,0 +1,27 @@
+package test.main;
+
+// MainClss 라는 이름의 클래스 만들기. {}가 클래스의 범위가 됨
+public class MainClass {
+	// main이라는 이름의 매소드를 클래스 안에 만들기
+	public static void main(String[] args) {
+		// main()메소드 안에 있는 코드를 실행하려면 run 버튼을 누르세요
+		System.out.println("Hello, World!");
+		System.out.println("성완");
+
+		// let num = 10;
+		int num = 10;
+		num = 20;
+		num = 30;
+		num = 111;
+		double num2 = 10.1;
+		num2 = 20.1;
+		int a = 10; 
+
+		boolean isRun = true;
+		isRun = false;
+
+		String myName = "김구라";
+		myName = "해골";
+
+	}
+}
