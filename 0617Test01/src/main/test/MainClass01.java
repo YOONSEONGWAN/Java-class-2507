1e58081627929a786ce67c95e05d378099d2e969
seongwan <wannietheyoon@naver.com>
2025-06-18 18:14:07 +0900

    a

--- 0617Test01/src/main/test/MainClass01.java
+++ 0617Test01/src/main/test/MainClass01.java
@@ -1,5 +1,19 @@
 package main.test;
 
 public class MainClass01 {
-
+	public static void main(String[] args) {
+		boolean result1 = 10==10;
+		boolean result2 = 10!=10;
+		
+		boolean result10 = false||false ;
+		boolean result20 = false||true;
+		boolean result30 = true&&true;
+		boolean result40 = true&&false;
+		
+		long sum=1;
+		for(long i=1; i<=25; i++) {
+			sum/=i;
+		}
+		System.out.println("숫자의 곱:"+sum);
+	}
 }
